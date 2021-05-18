b512020a38f89c7cbc7cb42160bf071c6fe96f92
Rengaraj <rengaraj.anandan@ziffity.com>
2021-05-18 11:46:05 +0530

    Added Modified & deleted files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import Todo from "./components/todo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div class="row g-2">
+        <div class="col-3"></div>
+        <div class="col-6">
+          <div class="p-3 mb-2 m-3">
+            <Todo />
+          </div>
+        </div>
+        <div className="col-3"></div>
+      </div>
     </div>
   );
 }
